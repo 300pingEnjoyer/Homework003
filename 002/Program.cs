872a4198d4e330b6ed266550cdbb92fced8c99b0
300pingEnjoyer <va5gromov@yandex.ru>
2022-11-23 10:40:41 +0700

    task 002 complete

---
+++ 002/Program.cs
@@ -0,0 +1,20 @@
+﻿double GetLens(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double len = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
+    double len2 = Math.Round(len, 2);
+    return len2;
+}
+Console.WriteLine("input x1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input x2");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input y2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input z1");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input z2");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(GetLens(x1, y1, z1, x2, y2, z2));
