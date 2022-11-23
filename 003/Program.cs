d55d94562b78b17efb6cbb7e6656f72cdb6789cc
300pingEnjoyer <va5gromov@yandex.ru>
2022-11-23 10:51:52 +0700

    task 003 complete

---
+++ 003/Program.cs
@@ -0,0 +1,10 @@
+﻿void SqrAll(int N)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        Console.WriteLine($"{i} -> {i * i * i}");
+    }
+}
+Console.WriteLine("input N");
+int N = Convert.ToInt32(Console.ReadLine());
+SqrAll(N);
